f7b2242546d801b2bf762215a603cf4924df6f9a
venkat225-Git <sakavenkatasai@gmail.com>
2025-10-29 21:17:44 +0530

    temp: disable DB write for debug

--- server/controllers/historyController.js
+++ server/controllers/historyController.js
@@ -6,7 +6,7 @@ const fetchhistory = async () =>{
     try {
         const data = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1") 
         const outcome = await data.json()
-        await History.insertMany(outcome); 
+        //await History.insertMany(outcome); 
         console.log(" Data inserted successfully!");
         
         
